44056d9a23f56ea9b8f26e721a8dffc36304ce30
tsatse <djo@pxv_001.(none)>
2013-03-15 01:53:13 +0100

    cleaned up the code

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 js1k-2013
 =========
+check out the 'original' branch for the original entry's source code
 
 The unminified code for my js13 for the 2013 competition : 'spring'
 A procedurally generated tree that grows and waves in the wind.
--- entry.js
+++ entry.js
@@ -1,108 +1,114 @@
 ﻿function makeJitter(s) {
-    return s+m.floor(r()*(50)-25);
+    return s + Math.floor(Math.random() * 25);
 }
 
-function drawBranch(x, y, tree, ag, length) {
-    a.fillStyle = 'rgb(' + tree.t.join(',') +')';
+function drawBranch(x, y, tree, angle, length) {
+    a.fillStyle = 'rgb(' + tree.tint.join(',') + ')';
     a.save();
-    a.t(x, y);
-    a.rotate(ag);
+    a.translate(x, y);
+    a.rotate(angle);
     a.fillStyle = '#ffe';
-    a.f(0, -tree.thickness/2-0.5, length, tree.thickness);
-    a.fillStyle = 'rgb(' + tree.t.join(',') +')';
-    a.f(0, -tree.thickness/2, length, tree.thickness);
-    a.r();
-    drawLeaves(x, y, tree, ag);
+    a.fillRect(0, -tree.thickness / 2 - 0.5, length, tree.thickness);
+    a.fillStyle = 'rgb(' + tree.tint.join(',') + ')';
+    a.fillRect(0, -tree.thickness / 2, length, tree.thickness);
+    a.restore();
+    drawLeaves(x, y, tree, angle);
 }
 
-function drawLeaves(x, y, tree, ag, length) {
+function drawLeaves(x, y, tree, angle) {
     a.save();
-    a.t(x, y);
-    a.rotate(ag);
-        a.t(5*g, 0);
+    a.translate(x, y);
+    a.rotate(angle);
+    a.translate(5*growth, 0);
     for(i = 0 ; i < tree.leaves.length ; i++) {
         var leaf = tree.leaves[i];
-        a.fillStyle = 'rgba(' + leaf.join(',')+')';
-        a.f(
-            -2*g,
-            ((i%2)?-4*g:0)+1*m.cos(p[0]*16+i*m.PI/2),
-            (4+m.sin(p[0]*16+i*m.PI/2))*g,
-            (4-m.cos(p[0]*16+i*m.PI/2))*g);
-        a.t(10*g, 0);
+        a.fillStyle = 'rgba(' + leaf.join(',') + ')';
+        a.fillRect(
+            -2 * growth,
+            ((i % 2) ? -4 * growth : 0) + 1 * Math.cos(phase[0] * 16 + i * Math.PI / 2),
+            ( 4 + Math.sin(phase[0] * 16 + i * Math.PI / 2)) * growth,
+            ( 4 - Math.cos(phase[0] * 16 + i * Math.PI / 2)) * growth);
+        a.translate(10 * growth, 0);
     }
-    a.r();
+    a.restore();
 }
-function drawTree(x, y, tree, ag) {
-    var angle=tree.angle+ag+.01*m.sin(p[tree.level]+tree.phaseOffset);
-    g=m.max(g, m.min(1,g+r()*.0001-.000085));
-    var l=tree.l*g;
-    drawBranch(x,y,tree,angle,l);
+function drawTree(x, y, tree, angle) {
+    var angle = tree.angle + angle + .01 * Math.sin(phase[tree.level] + tree.phaseOffset);
+    growth = Math.max(growth, Math.min(1, growth + Math.random() * .0001 - .000085));
+    var length = tree.length * growth;
+    drawBranch(x, y, tree, angle, length);
     
-    for(var i=0;i<tree.c.length;i++) {
+    for(var i = 0 ; i < tree.children.length ; i++) {
         drawTree(
-            x+l*m.cos(angle),
-            y+l*m.sin(angle),
-            tree.c[i],angle
+            x + length * Math.cos(angle),
+            y + length * Math.sin(angle),
+            tree.children[i], angle
         );
     }
 }
-function buildTreeData(parent,level) {
+function buildTreeData(parent, level) {
     var result = {
-        level:level,
-        leaves:[],
-        t:[
-        makeJitter(95),
-        makeJitter(79),
-        makeJitter(41)
+        level: level,
+        leaves: [],
+        tint: [
+            makeJitter(95),
+            makeJitter(79),
+            makeJitter(41)
         ],
-        thickness:!level?3:parent.thickness - .5,
-        phaseOffset:r()*2,
-        c:[]
+        thickness: !level ? 3 : (parent.thickness - .5),
+        phaseOffset: Math.random() * 2,
+        children: []
     };
-    result.angle=level?(r()-.5)*m.PI/2:0;
-    result.beta=r()*.8;
-    result.l=level?parent.l*(1-.4*r()):50;
-    if(level>1){
-        for(k=5;k<result.l;k+=10){
+    result.angle = level ? (Math.random() - .5) * Math.PI / 2:0;
+    result.beta = Math.random() * .8;
+    result.length = level ? parent.length * (1 - .4 * Math.random()):50;
+    if(level > 1) {
+        for(k = 5 ; k < result.length ; k += 10){
             result.leaves.push([
                 makeJitter(142),
                 makeJitter(160),
                 makeJitter(68),
-                (((k-5)/10)%2)?.3:.75
+                (((k - 5) / 10) % 2) ? .3 : .75
                 ]);
         }
     }
     if(level < 5) {
-        result.c.push(buildTreeData(result, level+1),buildTreeData(result, level+1),buildTreeData(result, level+1));
-        result.level>1?result.c.push(buildTreeData(result,level+1),buildTreeData(result,level+1)):0;
+        result.children.push(
+            buildTreeData(result, level + 1),
+            buildTreeData(result, level + 1),
+            buildTreeData(result, level + 1)
+        );
+        if(result.level > 1) {
+            result.children.push(
+                buildTreeData(result, level + 1),
+                buildTreeData(result, level + 1)
+            );
+        }
     }
     return result;
 }
 
 
 function draw() {
-    for(A=p.length-1;A>=0;A--){
-        p[A+1] = p[A];
+    var i,
+        gradient;
+    for(i = p.length - 1 ; i >= 0 ; i--) {
+        phase[i + 1] = phase[i];
     }
-    p[0] += .1;
-    d=a.createLinearGradient(0,0,0,h);
-    d.D=d.addColorStop
-    d.D(0,'#26b');
-    d.D(.7,'#69e');
-    d.D(.99,'#fcb');
-    d.D(1,'#753');
-    a.fillStyle=d;
-    a.f(0,0,w,h);
-    drawTree(150,h,u,-m.PI/2);
+    phase[0] += .1;
+    gradient = a.createLinearGradient(0, 0, 0, h);
+    gradient.addColorStop(0,'#26b');
+    gradient.addColorStop(.7,'#69e');
+    gradient.addColorStop(.99,'#fcb');
+    gradient.addColorStop(1,'#753');
+    a.fillStyle = gradient;
+    a.fillRect(0, 0, canvas.width, canvas.height);
+    drawTree(150, height, u, -Math.PI/2);
 }
-m=Math
-a.t=a.translate
-a.f=a.fillRect
-a.r=a.restore
-r=m.random
-w=c.width=500;
-h=c.height=500;
-p = [0,0,0,0,0,0,0,0];
-g = .001;
+
+canvas.width = 500;
+canvas.height = 500;
+phase = [0, 0, 0, 0, 0, 0, 0, 0];
+var growth = .001;
 u = buildTreeData(null, 0);
 setInterval(draw, 42);
